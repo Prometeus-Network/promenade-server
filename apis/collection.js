082314cf8cf1e4d5b4a6b2c12dcb3d7f594a4426
Gosha Skryuchenkov <gosha.skryuchenkov@gmail.com>
2021-09-30 15:48:30 +0300

    feat: changed royalty to 0 for test

--- apis/collection.js
+++ apis/collection.js
@@ -309,7 +309,7 @@ router.post("/reviewApplication", admin_auth, async (req, res) => {
     } else if (status == 1) {
       // update smart contract for royalty
       let feeRecipient = toLowerCase(collection.feeRecipient);
-      let royalty = parseInt(collection.royalty * 100);
+      let royalty = parseInt(collection.royalty * 0);
       let creator = collection.owner;
 
       // validate fee receipient to be a valid erc20 address
