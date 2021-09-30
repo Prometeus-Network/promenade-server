8aa0a2e8dd3ea13a8f927af71b67bd96310e7aac
Maxim <maxvalenko@gmail.com>
2021-09-30 14:46:37 +0300

    admin address to lowercase

--- apis/middleware/auth.admin.js
+++ apis/middleware/auth.admin.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const extractAddress = require("../../services/address.utils");
 const AdminAddresses = require("../../config/adminAddress");
+const toLowerCase = require("../../utils/utils");
 
 const admin_auth = (req, res, next) => {
   try {
     let address = extractAddress(req, res);
-    if (AdminAddresses.includes(address)) {
+    if (AdminAddresses.map(toLowerCase).includes(toLowerCase(address))) {
       next();
     } else
       return res.status(400).json({
